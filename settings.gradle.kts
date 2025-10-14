36d4618cdd0fcbabd7bc2ceb7c6395092598c9f1
CarlosDzRdz <cadr2627@gmail.com>
2025-10-14 17:41:41 -0600

    App reloj inicial

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,8 +1,8 @@
 pluginManagement {
     repositories {
         google {
-            content {
-                includeGroupByRegex("com\\.android.*")
+            content {includeGroupByRegex(
+                "com\\.android.*")
                 includeGroupByRegex("com\\.google.*")
                 includeGroupByRegex("androidx.*")
             }
@@ -21,4 +21,6 @@ dependencyResolutionManagement {
 
 rootProject.name = "Vendeta"
 include(":app")
- 
+
+include(":wear")
+include(":wear")
